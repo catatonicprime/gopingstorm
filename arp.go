fe9a537d9a4ae8036f92841709051e33a4b84bd1
catatonicprime <catatonicprime@gmail.com>
2024-07-27 15:48:05 +0000

    Testing adding and deleting hosts from ARP cache

--- arp.go
+++ arp.go
@@ -1,63 +1,66 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "time"
+	"fmt"
+	"net"
+	"time"
 )
 
 type Host struct {
-    IP        net.IP
-    Name      string
-    MAC       net.HardwareAddr
-    Timestamp time.Time
+	IP        net.IP
+	Comment   string
+	MAC       net.HardwareAddr
+	Timestamp time.Time
 }
 
-var ipToHostMap = map[string]Host{
-    "192.168.1.1": {
-        IP:        net.ParseIP("192.168.1.1"),
-        Name:      "Router",
-        MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E},
-        Timestamp: time.Now(),
-    },
-    "192.168.1.2": {
-        IP:        net.ParseIP("192.168.1.2"),
-        Name:      "Laptop",
-        MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5F},
-        Timestamp: time.Now(),
-    },
+var arpCache = map[string]Host{
+	"192.168.1.1": {
+		IP:        net.ParseIP("192.168.1.1"),
+		Comment:   "Router",
+		MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E},
+		Timestamp: time.Now(),
+	},
+	"192.168.1.2": {
+		IP:        net.ParseIP("192.168.1.2"),
+		Comment:   "Laptop",
+		MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5F},
+		Timestamp: time.Now(),
+	},
 }
 
-func AddHost(ipStr, macStr, name string) error {
-    ip := net.ParseIP(ipStr)
-    if ip == nil {
-        return fmt.Errorf("invalid IP address: %s", ipStr)
-    }
+func AddHost(ipStr, macStr, comment string) error {
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return fmt.Errorf("invalid IP address: %s", ipStr)
+	}
 
-    mac, err := net.ParseMAC(macStr)
-    if err != nil {
-        return fmt.Errorf("invalid MAC address: %v", err)
-    }
+	mac, err := net.ParseMAC(macStr)
+	if err != nil {
+		return fmt.Errorf("invalid MAC address: %v", err)
+	}
 
-    host := Host{
-        IP:        ip,
-        Name:      name,
-        MAC:       mac,
-        Timestamp: time.Now(),
-    }
+	host := Host{
+		IP:        ip,
+		Comment:   comment,
+		MAC:       mac,
+		Timestamp: time.Now(),
+	}
 
-    ipToHostMap[ipStr] = host
-    return nil
+	arpCache[ipStr] = host
+	return nil
+}
+
+func DeleteHost(ipStr string) {
+	delete(arpCache, ipStr)
 }
 
 func main() {
-    err := AddHost("192.168.1.3", "00:1A:2B:3C:4D:60", "Smartphone")
-    if err != nil {
-        fmt.Printf("Error adding host: %v\n", err)
-    }
+	err := AddHost("192.168.1.3", "00:1A:2B:3C:4D:60", "Smartphone")
+	if err != nil {
+		fmt.Printf("Error adding host: %v\n", err)
+	}
 
-    for ip, host := range ipToHostMap {
-        fmt.Printf("IP: %s, Host: %+v\n", ip, host)
-    }
+	for ip, host := range arpCache {
+		fmt.Printf("IP: %s, Host: %+v\n", ip, host)
+	}
 }
-
--- arp_test.go
+++ arp_test.go
@@ -1,63 +1,69 @@
 package main
 
 import (
-    "net"
-    "testing"
-    "time"
+	"net"
+	"testing"
+	"time"
 )
 
-func TestAddHost(t *testing.T) {
-    // Test case 1: Valid inputs
-    ipStr := "192.168.1.3"
-    macStr := "00:1A:2B:3C:4D:60"
-    name := "Smartphone"
+func TestAddDeleteHost(t *testing.T) {
+	// Test case 1: Valid inputs
+	ipStr := "192.168.1.3"
+	macStr := "00:1A:2B:3C:4D:60"
+	comment := "Smartphone"
 
-    err := AddHost(ipStr, macStr, name)
-    if err != nil {
-        t.Errorf("AddHost returned an error: %v", err)
-    }
+	err := AddHost(ipStr, macStr, comment)
+	if err != nil {
+		t.Errorf("AddHost returned an error: %v", err)
+	}
 
-    expectedIP := net.ParseIP(ipStr)
-    if expectedIP == nil {
-        t.Fatalf("Invalid test case: could not parse IP %s", ipStr)
-    }
+	expectedIP := net.ParseIP(ipStr)
+	if expectedIP == nil {
+		t.Fatalf("Invalid test case: could not parse IP %s", ipStr)
+	}
 
-    expectedHost := Host{
-        IP:        expectedIP,
-        Name:      name,
-        MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x60},
-        Timestamp: time.Now(),
-    }
+	expectedHost := Host{
+		IP:        expectedIP,
+		Comment:   comment,
+		MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x60},
+		Timestamp: time.Now(),
+	}
 
-    // Retrieve the added host from the map
-    addedHost, ok := ipToHostMap[ipStr]
-    if !ok {
-        t.Fatalf("Host with IP %s was not added to the map", ipStr)
-    }
+	// Retrieve the added host from the map
+	addedHost, ok := arpCache[ipStr]
+	if !ok {
+		t.Fatalf("Host with IP %s was not added to the map", ipStr)
+	}
 
-    // Compare the added host with the expected host
-    if addedHost.IP.String() != expectedHost.IP.String() ||
-        addedHost.Name != expectedHost.Name ||
-        addedHost.MAC.String() != expectedHost.MAC.String() {
-        t.Errorf("AddHost did not add the host correctly. Expected %+v, got %+v", expectedHost, addedHost)
-    }
+	// Compare the added host with the expected host
+	if addedHost.IP.String() != expectedHost.IP.String() ||
+		addedHost.Comment != expectedHost.Comment ||
+		addedHost.MAC.String() != expectedHost.MAC.String() {
+		t.Errorf("AddHost did not add the host correctly. Expected %+v, got %+v", expectedHost, addedHost)
+	}
+
+	// Delete the host too
+	DeleteHost(ipStr)
+	_, ok = arpCache[ipStr]
+	if ok {
+		t.Errorf("Failed to delete host from arpCache!")
+	}
 }
 
 func TestAddHost_InvalidIP(t *testing.T) {
-    // Test case 2: Invalid IP
-    invalidIPStr := "invalid_ip_address"
-    err := AddHost(invalidIPStr, "00:1A:2B:3C:4D:60", "Test")
-    if err == nil {
-        t.Errorf("AddHost with invalid IP %s did not return an error", invalidIPStr)
-    }
+	// Test case 2: Invalid IP
+	invalidIPStr := "invalid_ip_address"
+	err := AddHost(invalidIPStr, "00:1A:2B:3C:4D:60", "Test")
+	if err == nil {
+		t.Errorf("AddHost with invalid IP %s did not return an error", invalidIPStr)
+	}
 }
 
 func TestAddHost_InvalidMAC(t *testing.T) {
-    // Test case 2: Invalid IP
-    invalidMACStr := "invalid_mac_address"
-    err := AddHost("192.168.7.1", invalidMACStr, "Test")
-    if err == nil {
-        t.Errorf("AddHost with invalid MAC %s did not return an error", invalidMACStr)
-    }
+	// Test case 2: Invalid IP
+	invalidMACStr := "invalid_mac_address"
+	err := AddHost("192.168.7.1", invalidMACStr, "Test")
+	if err == nil {
+		t.Errorf("AddHost with invalid MAC %s did not return an error", invalidMACStr)
+	}
 }
-
