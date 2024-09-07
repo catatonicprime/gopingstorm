4c674eb778767a8f7852de40c7e1f78f11a20d8c
catatonicprime <catatonicprime@gmail.com>
2024-09-07 17:27:20 +0000

    Adding event logging. Added cache expiration. Trying out go doc comments.

--- arp.go
+++ arp.go
@@ -1,33 +1,57 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net"
 	"time"
 )
 
+// Host represents a host on a network.
 type Host struct {
-	IP        net.IP
-	Comment   string
-	MAC       net.HardwareAddr
+	// IP is the IP address of the host.
+	IP net.IP
+
+	// MAC is the MAC address of the host.
+	MAC net.HardwareAddr
+
+	// Timestamp is the time the ARP host was observed
+	Timestamp time.Time
+
+	// Comment is additional user-defined annotating information for the host.
+	Comment string
+}
+
+//TODO: Create a lifetime database of hosts observed too.
+
+// arpCache is the current cache of hosts that has been discovered.
+var arpCache map[string]Host = make(map[string]Host)
+
+// Event represents ARP events like a host changing MAC. In the future additional events
+// such as the initial observeration of a host.
+type Event struct {
+	// Description describes the Event
+	Description string
+
+	// Timestamp is the time the event occurred
 	Timestamp time.Time
 }
 
-var arpCache = map[string]Host{
-	"192.168.1.1": {
-		IP:        net.ParseIP("192.168.1.1"),
-		Comment:   "Router",
-		MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E},
-		Timestamp: time.Now(),
-	},
-	"192.168.1.2": {
-		IP:        net.ParseIP("192.168.1.2"),
-		Comment:   "Laptop",
-		MAC:       net.HardwareAddr{0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5F},
-		Timestamp: time.Now(),
-	},
+// arpEvents is a list of Events that have occurred.
+var arpEvents []Event = make([]Event, 0)
+
+// AddEvent adds an ARP event ot the arpEvents list and logs the event.
+func AddEvent(description string, timestamp time.Time) {
+	event := Event{
+		Description: description,
+		Timestamp:   timestamp,
+	}
+	log.Printf(fmt.Sprintf("%s: %s", timestamp.Format(time.RFC3339), event.Description))
+	arpEvents = append(arpEvents, event)
 }
 
+// AddHost adds a new host record to the arpCache with a timestamp of the time of addition.
 func AddHost(ipStr, macStr, comment string) error {
 	ip := net.ParseIP(ipStr)
 	if ip == nil {
@@ -43,13 +67,30 @@ func AddHost(ipStr, macStr, comment string) error {
 		IP:        ip,
 		Comment:   comment,
 		MAC:       mac,
-		Timestamp: time.Now(),
+		Timestamp: time.Now(), // TODO: assess if this timestamp should be derived from a packet timestamp instead.
 	}
 
+	// Log changes to arpCache if necessary
+	if currentHost, exists := arpCache[ipStr]; exists && !bytes.Equal(currentHost.MAC, host.MAC) {
+		AddEvent(fmt.Sprintf("Host MAC changed from %s to %s\n", currentHost.MAC, host.MAC), host.Timestamp)
+	}
+
+	//Update the cache regardless
 	arpCache[ipStr] = host
 	return nil
 }
 
+// ExpireHosts will delete hosts that are older than the time _since_.
+func ExpireHosts(since time.Time) {
+	for key, host := range arpCache {
+		if host.Timestamp.Before(since) {
+			AddEvent(fmt.Sprintf("Host %s has expired", key), time.Now())
+			delete(arpCache, key)
+		}
+	}
+}
+
+// DeleteHost deletes a specific host
 func DeleteHost(ipStr string) {
 	delete(arpCache, ipStr)
 }
--- arp_test.go
+++ arp_test.go
@@ -6,20 +6,50 @@ import (
 	"time"
 )
 
+func setup() {
+	arpCache = make(map[string]Host)
+	arpEvents = make([]Event, 0)
+}
+
+func TestAddHost_InvalidIP(t *testing.T) {
+	setup()
+	// Test case 2: Invalid IP
+	invalidIPStr := "invalid_ip_address"
+	err := AddHost(invalidIPStr, "00:1A:2B:3C:4D:60", "Test")
+	if err == nil {
+		t.Errorf("AddHost with invalid IP %s did not return an error", invalidIPStr)
+	}
+}
+
+func TestAddHost_InvalidMAC(t *testing.T) {
+	setup()
+	// Test case 2: Invalid IP
+	invalidMACStr := "invalid_mac_address"
+	err := AddHost("192.168.7.1", invalidMACStr, "Test")
+	if err == nil {
+		t.Errorf("AddHost with invalid MAC %s did not return an error", invalidMACStr)
+	}
+}
+
 func TestAddDeleteHost(t *testing.T) {
+	setup()
+	if len(arpCache) != 0 {
+		t.Errorf("arpCache length is unexpected initial length!\n\tExpected Length: 0\n\tActual Length: %d", len(arpCache))
+	}
+
 	// Test case 1: Valid inputs
 	ipStr := "192.168.1.3"
 	macStr := "00:1A:2B:3C:4D:60"
 	comment := "Smartphone"
 
-	err := AddHost(ipStr, macStr, comment)
-	if err != nil {
-		t.Errorf("AddHost returned an error: %v", err)
-	}
-
 	expectedIP := net.ParseIP(ipStr)
 	if expectedIP == nil {
 		t.Fatalf("Invalid test case: could not parse IP %s", ipStr)
+	}
+
+	err := AddHost(ipStr, macStr, comment)
+	if err != nil {
+		t.Errorf("AddHost returned an error: %v", err)
 	}
 
 	expectedHost := Host{
@@ -35,6 +65,11 @@ func TestAddDeleteHost(t *testing.T) {
 		t.Fatalf("Host with IP %s was not added to the map", ipStr)
 	}
 
+	// Ensure we didn't somehow also add a second record
+	if len(arpCache) != 1 {
+		t.Errorf("arpCache length is unexpected length after add!\n\tExpected Length: 1\n\tActual Length: %d", len(arpCache))
+	}
+
 	// Compare the added host with the expected host
 	if addedHost.IP.String() != expectedHost.IP.String() ||
 		addedHost.Comment != expectedHost.Comment ||
@@ -48,22 +83,78 @@ func TestAddDeleteHost(t *testing.T) {
 	if ok {
 		t.Errorf("Failed to delete host from arpCache!")
 	}
-}
-
-func TestAddHost_InvalidIP(t *testing.T) {
-	// Test case 2: Invalid IP
-	invalidIPStr := "invalid_ip_address"
-	err := AddHost(invalidIPStr, "00:1A:2B:3C:4D:60", "Test")
-	if err == nil {
-		t.Errorf("AddHost with invalid IP %s did not return an error", invalidIPStr)
+	if len(arpCache) != 0 {
+		t.Errorf("arpCache length is unexpected length after delete!\n\tExpected Length: 0\n\tActual Length: %d", len(arpCache))
 	}
 }
 
-func TestAddHost_InvalidMAC(t *testing.T) {
-	// Test case 2: Invalid IP
-	invalidMACStr := "invalid_mac_address"
-	err := AddHost("192.168.7.1", invalidMACStr, "Test")
-	if err == nil {
-		t.Errorf("AddHost with invalid MAC %s did not return an error", invalidMACStr)
+func TestArpEvents(t *testing.T) {
+	setup()
+	// Assert arpEvents is a 0 length list
+	if len(arpEvents) != 0 {
+		t.Errorf("arpEvents length is unexpected length!\n\tExpected Length: 0\n\tActual Length: %d", len(arpEvents))
+	}
+
+	ipStr := "192.168.1.3"
+	macStr := "00:1A:2B:3C:4D:60"
+	comment := "Smartphone"
+
+	expectedIP := net.ParseIP(ipStr)
+	if expectedIP == nil {
+		t.Fatalf("Invalid test case: could not parse IP %s", ipStr)
+	}
+
+	// Add the same host twice, this should *not* generate an event.
+	err := AddHost(ipStr, macStr, comment)
+	if err != nil {
+		t.Errorf("AddHost returned an error: %v", err)
+	}
+	err = AddHost(ipStr, macStr, comment)
+	if err != nil {
+		t.Errorf("AddHost returned an error: %v", err)
+	}
+	if len(arpEvents) != 0 {
+		t.Errorf("arpEvents length is unexpected length!\n\tExpected Length: 0\n\tActual Length: %d", len(arpEvents))
+	}
+
+	// Add the same host again, but with a new MAC, this *should* generate an event
+	macStr = "00:1A:2B:3C:4D:61"
+	err = AddHost(ipStr, macStr, comment)
+	if err != nil {
+		t.Errorf("AddHost returned an error: %v", err)
+	}
+	if len(arpEvents) != 1 {
+		t.Errorf("arpEvents length is unexpected length!\n\tExpected Length: 1\n\tActual Length: %d", len(arpEvents))
+	}
+}
+
+func TestExpireHosts(t *testing.T) {
+	setup()
+	// Assert arpEvents is a 0 length list
+	if len(arpEvents) != 0 {
+		t.Errorf("arpEvents length is unexpected length!\n\tExpected Length: 0\n\tActual Length: %d", len(arpEvents))
+	}
+
+	ipStr := "192.168.1.3"
+	macStr := "00:1A:2B:3C:4D:60"
+	comment := "Smartphone"
+
+	expectedIP := net.ParseIP(ipStr)
+	if expectedIP == nil {
+		t.Fatalf("Invalid test case: could not parse IP %s", ipStr)
+	}
+
+	// Add the same host twice, this should *not* generate an event.
+	err := AddHost(ipStr, macStr, comment)
+	if err != nil {
+		t.Errorf("AddHost returned an error: %v", err)
+	}
+
+	ExpireHosts(time.Now())
+	if len(arpEvents) != 1 {
+		t.Errorf("arpEvents length is unexpected length!\n\tExpected Length: 1\n\tActual Length: %d", len(arpEvents))
+	}
+	if len(arpCache) != 0 {
+		t.Errorf("arpCache length is unexpected length after expire!\n\tExpected Length: 0\n\tActual Length: %d", len(arpCache))
 	}
 }
